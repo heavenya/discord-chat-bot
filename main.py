ac377b7a496a7491febba3d486ff55c4cb044666
Travis Mitchell <tmitch5151@gmail.com>
2021-11-18 16:31:12 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,109 @@
+import discord
+import os
+import requests
+import json
+import random
+from replit import db
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+key_words = ["feedback", "stuff"]
+
+starter_encouragements = [
+  "feel free to check out upcoming ecommerce products at https://discord.gg/UTK3UWUfDz",
+
+   "feel free to check out Heavenya Christian Felllowship App at https://discord.gg/P69xMxq9pb",
+
+   "feel free to check out Animation Videos at https://discord.gg/bmua6PWYNE",
+
+   "feel free to check out Inspire at  https://discord.gg/a9pUU3SgsP",
+
+   "feel free to check out Small Group at  https://discord.gg/nHBednDTdN",
+
+   "feel free to check out My Church at   https://discord.gg/nHUNfhpnE9",
+  
+   "feel free to check out nfts at https://discord.gg/yCSYPVYmm3",
+
+   "feel free to check out text at https://discord.gg/veNDdaT3by",
+
+   "feel free to check out Chat Bot at https://discord.gg/BR7taYc4fB"
+]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragment(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+    db["encouragements"] = encouragements
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if db["responding"]:
+    options = starter_encouragements
+    if "encouragements" in db.keys():
+      options = options + db["encouragements"]
+
+    if any(word in msg for word in key_words):
+      await message.channel.send(random.choice(options))
+
+  if msg.startswith("$new"):
+    encouraging_message = msg.split("$new ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+  if msg.startswith("$del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("$del",1)[1])
+      delete_encouragment(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith("$list"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith("$responding"):
+    value = msg.split("$responding ",1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Responding is on.")
+    else:
+      db["responding"] = False
+      await message.channel.send("Responding is off.")
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
